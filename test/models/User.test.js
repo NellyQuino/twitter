68ad56294d6ddea294fecd32f493f3386405ef67
NellyQuino <71736979+NellyQuino@users.noreply.github.com>
2022-04-21 15:47:19 -0700

    Agregando requerimiento  2 (modificacion y respectiva prueba)

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,9 +1,15 @@
-const { TestWatcher } = require("jest")
+const User = require('./../../app/models/User')
 
-describe("Prueba de jest", () => {
-    test("Prueba 1", () => {
-        expect(10).toBe(9)
+describe("Test para User", () => {
+    test("Creacion de User", () => {
+        const user = new User(1, "nellyquino", "Bio", "dateCreated", "lastUpdated")
+        expect(user.id).toBe(1)
 
+    })
+    test("Requerimiento 2: Fecha en atributos de User", () => {
+        const user = new User(2, "nellyjqh", "nelly", "Bio")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     })
 
 })
