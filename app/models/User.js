aa83aa11fd3572ad93ce6823ac3f56748893d365
NellyQuino <71736979+NellyQuino@users.noreply.github.com>
2022-04-22 12:26:43 -0700

    Agregando atributos a prueba 1

--- app/models/User.js
+++ app/models/User.js
@@ -1,11 +1,19 @@
 class User {
-    constructor(id, username, name, bio) {
+    constructor(id, username, name, bio, dateCreated, lastUpdated) {
         this.id = id
         this.username = username
         this.name = name
         this.bio = bio
-        this.dateCreated = new Date()
-        this.lastUpdated = new Date()
+        this.dateCreated = dateCreated
+        this.lastUpdated = lastUpdated
+            // this.dateCreated = new Date()
+            //this.lastUpdated = new Date()
+    }
+    get getUsername() {
+        return this.username
+    }
+    set setUsername(newUsername) {
+        this.username = newUsername
     }
 
 }
--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,14 +2,29 @@ const User = require('./../../app/models/User')
 
 describe("Test para User", () => {
     test("Creacion de User", () => {
-        const user = new User(1, "nellyquino", "Bio", "dateCreated", "lastUpdated")
-        expect(user.id).toBe(1)
+            const user = new User(1, "nellyquino", "nelly", "Bio", "dateCreated", "lastUpdated")
+            expect(user.id).toBe(1)
+            expect(user.username).toBe("nellyquino")
+            expect(user.name).toBe("nelly")
+            expect(user.bio).toBe("Bio")
+            expect(user.dateCreated).toBe("dateCreated")
+            expect(user.lastUpdated).toBe("lastUpdated")
 
-    })
-    test("Requerimiento 2: Fecha en atributos de User", () => {
+
+        })
+        /*test("Requerimiento 2: Fecha en atributos de User", () => {
         const user = new User(2, "nellyjqh", "nelly", "Bio")
         expect(user.dateCreated).not.toBeUndefined()
         expect(user.lastUpdated).not.toBeUndefined()
     })
+    test("Requerimiento 3: Agregando getters", () => {
+        const user = new User(2, "nellyjqh", "nelly", "Bio")
+        expect(user.getUsername).toBe("nellyjqh")
+    })
+    test("Requerimiento 4: Agregar setters", () => {
+        const user = new User(2, "nellyjqh", "nelly", "Bio")
+        user.setUsername = "jazminqh"
+        expect(user.username).toBe("jazminqh")
+    })/** */
 
 })
