41eafeb56da16ef45294544b3328dcf7e4cac0ca
NellyQuino <71736979+NellyQuino@users.noreply.github.com>
2022-04-25 13:56:51 -0700

    Requerimiento 3 UserView y sus prueba

--- app/Views/UserView.js
+++ app/Views/UserView.js
@@ -4,7 +4,11 @@ class UserView {
     static createUser(payload) {
         if (payload === null) {
             return { error: "payload no existe" }
+        } else if ((typeof payload.username !== "string" || payload.username === null) || (typeof payload.name !== "string" || payload.name === null) ||
+            (typeof payload.id !== "number" || payload.id === null)) {
+            return { error: "necesitas tener un valor válido " }
         }
+
     }
 
 }
--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -8,5 +8,12 @@ describe("Test for UserView", () => {
 
         expect(result.error).toMatch(/payload no existe/)
     })
+    test("Return an error object when try to create a new user with pyload with invalid properties", () => {
+        const payload = { username: null, name: 12, id: "id" }
+        const result = UserView.createUser(payload)
+
+        expect(result.error).toMatch(/necesitas tener un valor válido/)
+    });
+
 
 })
